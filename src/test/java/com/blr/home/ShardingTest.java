2f3f7f06d9feaeb4b1d9078c6125391317b5d503
niti9999 <43085638+niti9999@users.noreply.github.com>
2025-06-19 19:38:34 +0530

    commit

--- src/test/java/com/blr/home/ShardingTest.java
+++ src/test/java/com/blr/home/ShardingTest.java
@@ -3,6 +3,7 @@ package com.blr.home;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Tracing;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Paths;
@@ -16,12 +17,21 @@ public class ShardingTest {
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch();
             Page page = browser.newContext().newPage();
+
+            // Start tracing
+            page.context().tracing().start(new com.microsoft.playwright.Tracing.StartOptions()
+                .setScreenshots(true)
+                .setSnapshots(true));
+
             String url = page.navigate("https://www.example.com").url();
             assertEquals("https://www.example.com/", url);
 
             // Take a screenshot and save it to the "screenshots" directory
             page.screenshot(new Page.ScreenshotOptions()
                 .setPath(Paths.get("build/reports/screenshots/example.png")));
+
+            // Stop tracing and save the trace file
+            page.context().tracing().stop((Tracing.StopOptions) Paths.get("build/reports/trace.zip"));
         }
     }
 }
